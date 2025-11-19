da991284bbdfdc30c0228577e3f510c6cb3ef40c
Sergo_111 <168209084+BEBRA22222@users.noreply.github.com>
2025-11-19 11:14:11 +0300

    Add new repository
    new fle.html

---
+++ dron.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index')
+
+if __name__ == '__main__':
+    app.run(debug=True)
